6d34df4137479b1f520e6788ccbb9c0c429947f1
Eduardo <eduferramenta@gmail.com>
2024-10-11 16:33:07 -0300

    criacao do modelo de fornecedor

---
+++ ConstructionMVC/Models/Fornecedor.cs
@@ -0,0 +1,10 @@
+﻿namespace ConstructionMVC.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Fantasia { get; set; }
+        public string Responsavel { get; set; }
+    }
+}
