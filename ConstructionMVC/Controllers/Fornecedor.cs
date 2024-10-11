4f7dbaccfebd26d3a1d4824c343aba1f9f153262
Eduardo <eduferramenta@gmail.com>
2024-10-11 16:22:27 -0300

    initial commit

---
+++ ConstructionMVC/Controllers/Fornecedor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionMVC.Controllers
+{
+    public class Fornecedor : Controller
+    {
+        public String Index()
+        {
+            return "View()";
+        }
+    }
+}
---
+++ ConstructionMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConstructionMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
