6e2043be0871afb3b857223f73d8f6cd940c564a
Eduardo <eduferramenta@gmail.com>
2024-10-19 23:58:20 -0300

    Criacao do ModeloProduto

---
+++ ConstructionMVC/Models/ProdutoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ConstructionMVC.Models
+{
+	public class ProdutoModel
+	{
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public float Unidade { get; set; }
+        public float Peso { get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
--- ConstructionMVC/obj/Debug/net8.0/ConstructionMVC.AssemblyInfo.cs
+++ ConstructionMVC/obj/Debug/net8.0/ConstructionMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConstructionMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7637293bb227d9f78c156ee757c9ddd541be0df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbb1897bbd1e4415bb03eac6deff0dcb6634bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConstructionMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConstructionMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
