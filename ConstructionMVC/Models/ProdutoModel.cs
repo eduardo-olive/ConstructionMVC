e1816ff35388114c60371fdcae810d99a9b7e6bb
Eduardo <eduferramenta@gmail.com>
2024-10-22 16:05:56 -0300

    Criado chaves estrangerias para Fornecedor e Categoria

--- ConstructionMVC/Models/ProdutoModel.cs
+++ ConstructionMVC/Models/ProdutoModel.cs
@@ -5,28 +5,31 @@ namespace ConstructionMVC.Models
 {
 	public class ProdutoModel
 	{
-        [Key]
-        public int Id { get; set; }
-        
-        [Required(ErrorMessage = "O Campo {0} é de preenchimento obigatório.")]
-        public string Nome { get; set; }
-        
-        public string Descricao { get; set; }
-        
-        public float Unidade { get; set; }
-        
-        public float Peso { get; set; }
-        
-        [Required]
-        [ForeignKey(nameof(Fornecedor))]
-        public int IdFornecedor { get; set; }
-        
-        public FornecedorModel Fornecedor { get; set; }
-        
-        [Required]
-        [ForeignKey(nameof(Categoria))]
-        public int IdCategoria { get; set; }
-		
-        public CategoriaModel Categoria { get; set; }
-    }
+		[Key]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "O Campo {0} é de preenchimento obigatório.")]
+		public string Nome { get; set; }
+		[Display(Name = "Descrição")]
+		public string Descricao { get; set; }
+
+		public float Unidade { get; set; }
+
+		[Column(TypeName = "decimal(10,2)")]
+		public float Peso { get; set; }
+
+		[Required]
+		[Display(Name = "Fornecedor")]
+		public int IdFornecedor { get; set; }
+
+		[ForeignKey("IdFornecedor")]
+		public FornecedorModel Fornecedor { get; set; }
+
+		[Required]
+		[Display(Name = "Categoria")]
+		public int IdCategoria { get; set; }
+
+		[ForeignKey("IdCategoria")]
+		public CategoriaModel Categoria { get; set; }
+	}
 }
